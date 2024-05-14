45b3c5a2dac51a3f0001bbc094c68b02620fc6fe
Anzhelika-Mariia <angelika11hr@gmail.com>
2024-05-14 10:28:03 +0300

    Typo fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://anzhhm.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/lab2-hres/',
+  baseUrl: '/lab2-hress/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
